30b908a8452fbe61420903f20e87c01f44ec01ae
Gustavo Gonzalez <gustavo.gonzalez@openzeppelin.com>
2023-05-06 18:05:27 -0400

    replace array with pure function

--- src/Gas.sol
+++ src/Gas.sol
@@ -14,15 +14,6 @@ contract GasContract {
     mapping(address => uint256) public balances;
     mapping(address => uint256) public whitelist;
 
-    //TODO: Constant arrays are not supported, but maybe this can help https://ethereum.stackexchange.com/questions/66388/standard-work-around-for-using-a-solidity-constant-array-which-is-not-supported
-    address[5] public administrators = [
-        0x3243Ed9fdCDE2345890DDEAf6b083CA4cF0F68f2,
-        0x2b263f55Bf2125159Ce8Ec2Bb575C649f822ab46,
-        0x0eD94Bc8435F3189966a49Ca1358a55d871FC3Bf,
-        0xeadb3d065f8d15cc05e92594523516aD36d1c834,
-        contractOwner
-    ];
-
     //private
     mapping(address => uint256) private whiteListStruct;
 
@@ -74,5 +65,18 @@ contract GasContract {
         address sender
     ) public view returns (bool, uint256) {
         return (whiteListStruct[sender] > 0, whiteListStruct[sender]);
+    }
+
+    function administrators(
+        uint256 index
+    ) public pure returns (address _administrator) {
+        address[5] memory administrators = [
+            0x3243Ed9fdCDE2345890DDEAf6b083CA4cF0F68f2,
+            0x2b263f55Bf2125159Ce8Ec2Bb575C649f822ab46,
+            0x0eD94Bc8435F3189966a49Ca1358a55d871FC3Bf,
+            0xeadb3d065f8d15cc05e92594523516aD36d1c834,
+            contractOwner
+        ];
+        _administrator = administrators[index];
     }
 }
