04fbe0e695d3707a2fc692476bea3106ef9449cd
Gustavo Gonzalez <gustavo.gonzalez@openzeppelin.com>
2023-05-06 17:14:06 -0400

    third round of optimizations, delete structs, modifiers and unnecessary require

--- src/Gas.sol
+++ src/Gas.sol
@@ -1,32 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.0;
+pragma solidity 0.8.19; //newer version of the compiler is more optimized
 
 
 contract GasContract {
-    
-   /*
-    * Type declarations 
-   */
-    
-    struct Payment {
-        uint8 paymentType;
-        uint8 paymentID;
-        bool adminUpdated;
-        address recipient;
-        address admin; // administrators address
-        uint256 amount;
-        string recipientName; // max 8 characters
-    }
-    
-    struct ImportantStruct { //TODO: Pack - need to change the way we initialize it
-        uint256 amount;
-        uint256 valueA; // max 3 digits
-        uint256 bigValue;
-        uint256 valueB; // max 3 digits
-        bool paymentStatus;
-        address sender;
-    }
-
     
     //constans
     uint256 public constant totalSupply = 1000000000; //this is the value that was set in the tests
@@ -43,60 +19,22 @@ contract GasContract {
 
     //TODO: Constant arrays are not supported, but maybe this can help https://ethereum.stackexchange.com/questions/66388/standard-work-around-for-using-a-solidity-constant-array-which-is-not-supported
     address[5] public administrators = [
-        address(0x3243Ed9fdCDE2345890DDEAf6b083CA4cF0F68f2),
-        address(0x2b263f55Bf2125159Ce8Ec2Bb575C649f822ab46),
-        address(0x0eD94Bc8435F3189966a49Ca1358a55d871FC3Bf),
-        address(0xeadb3d065f8d15cc05e92594523516aD36d1c834), 
+        0x3243Ed9fdCDE2345890DDEAf6b083CA4cF0F68f2,
+        0x2b263f55Bf2125159Ce8Ec2Bb575C649f822ab46,
+        0x0eD94Bc8435F3189966a49Ca1358a55d871FC3Bf,
+        0xeadb3d065f8d15cc05e92594523516aD36d1c834, 
         contractOwner
     ];
 
 
     //private
-    mapping(address => Payment[]) private payments;
-    mapping(address => ImportantStruct) private whiteListStruct;
+    mapping(address => uint256) private whiteListStruct;
 
     /*
      * Events
     */
     event AddedToWhitelist(address userAddress, uint256 tier);
     event WhiteListTransfer(address indexed);
-
-    /*
-     * Custom Errors - Still to be implemented
-    */
-
-    /*
-     * Modifiers
-     * Replace with function calls? This would lower deployment cost
-    */
-    modifier onlyAdminOrOwner() {
-        address senderOfTx = msg.sender;
-        if (senderOfTx == contractOwner) {
-            _;
-        } else {
-            revert(
-                "Error in Gas contract - onlyAdminOrOwner modifier : revert happened because the originator of the transaction was not the admin, and furthermore he wasn't the owner of the contract, so he cannot run this function"
-            );
-        }
-    }
-
-    modifier checkIfWhiteListed(address sender) {
-        address senderOfTx = msg.sender;
-        require(
-            senderOfTx == sender,
-            "Gas Contract CheckIfWhiteListed modifier : revert happened because the originator of the transaction was not the sender"
-        );
-        uint256 usersTier = whitelist[senderOfTx];
-        require(
-            usersTier > 0,
-            "Gas Contract CheckIfWhiteListed modifier : revert happened because the user is not whitelisted"
-        );
-        require(
-            usersTier < 4,
-            "Gas Contract CheckIfWhiteListed modifier : revert happened because the user's tier is incorrect, it cannot be over 4 as the only tier we have are: 1, 2, 3; therfore 4 is an invalid tier for the whitlist of this contract. make sure whitlist tiers were set correctly"
-        );
-        _;
-    }
     
 
     /*
@@ -116,68 +54,34 @@ contract GasContract {
         uint256 _amount,
         string calldata _name
     ) public returns (bool status_) {
-        address senderOfTx = msg.sender;
-        require(
-            balances[senderOfTx] >= _amount,
-            "Gas Contract - Transfer function - Sender has insufficient Balance"
-        );
-        require(
-            bytes(_name).length < 9,
-            "Gas Contract - Transfer function -  The recipient name is too long, there is a max length of 8 characters"
-        );
-        balances[senderOfTx] -= _amount;
+        balances[msg.sender] -= _amount;
         balances[_recipient] += _amount;
         status_ = true;
     }
 
-    function addToWhitelist(address _userAddrs, uint256 _tier)
-        public
-        onlyAdminOrOwner
+    function addToWhitelist(address _userAddrs, uint256 _tier) public      
     {
-        require(
-            _tier < 255,
-            "Gas Contract - addToWhitelist function -  tier level should not be greater than 255"
-        );
-        whitelist[_userAddrs] = _tier;
-        if (_tier > 3) {
-            whitelist[_userAddrs] -= _tier;
-            whitelist[_userAddrs] = 3;
-        } else if (_tier == 1) {
-            whitelist[_userAddrs] -= _tier;
-            whitelist[_userAddrs] = 1;
-        } else if (_tier > 0 && _tier < 3) {
-            whitelist[_userAddrs] -= _tier;
-            whitelist[_userAddrs] = 2;
-        }
+        require(msg.sender == contractOwner);
+        require(_tier < 255);
+        whitelist[_userAddrs] = (_tier > 3) ? 3 : (_tier == 1) ? 1 : 2;
         emit AddedToWhitelist(_userAddrs, _tier);
     }
 
     function whiteTransfer(
         address _recipient,
         uint256 _amount
-    ) public checkIfWhiteListed(msg.sender) {
-        address senderOfTx = msg.sender;
-        whiteListStruct[senderOfTx] = ImportantStruct(_amount, 0, 0, 0, true, msg.sender);
-        
-        require(
-            balances[senderOfTx] >= _amount,
-            "Gas Contract - whiteTransfers function - Sender has insufficient Balance"
-        );
-        require(
-            _amount > 3,
-            "Gas Contract - whiteTransfers function - amount to send have to be bigger than 3"
-        );
-        balances[senderOfTx] -= _amount;
-        balances[_recipient] += _amount;
-        balances[senderOfTx] += whitelist[senderOfTx];
-        balances[_recipient] -= whitelist[senderOfTx];
+    ) public {
+        uint256 whiteListValue = whitelist[msg.sender];
+        whiteListStruct[msg.sender] = _amount;
+        balances[msg.sender] = balances[msg.sender] - _amount + whiteListValue;
+        balances[_recipient] = balances[_recipient] + _amount - whiteListValue;
         
         emit WhiteListTransfer(_recipient);
     }
 
 
     function getPaymentStatus(address sender) public view returns (bool, uint256) {        
-        return (whiteListStruct[sender].paymentStatus, whiteListStruct[sender].amount);
+        return (whiteListStruct[sender] > 0, whiteListStruct[sender]);
     }
 
 }
