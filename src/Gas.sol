05917de5d00f86dda8ef6ff914b336f61e4368d5
Gustavo Gonzalez <gustavo.gonzalez@openzeppelin.com>
2023-05-07 23:05:37 -0400

    remove storage variables and change with functions

--- src/Gas.sol
+++ src/Gas.sol
@@ -4,79 +4,75 @@ pragma solidity 0.8.19; //newer version of the compiler is more optimized
 contract GasContract {
     //constans
     uint256 public constant totalSupply = 1000000000; //this is the value that was set in the tests
-    uint256 public constant tradePercent = 12;
-    address public constant contractOwner = address(0x1234); //this is what is set in the test(vm.prank(owner))
+    address public constant contractOwner =
+        0x0000000000000000000000000000000000001234; //this is what is set in the test(vm.prank(owner))
 
     /*
      * State variables
      */
-    //public
-    mapping(address => uint256) public balances;
-    mapping(address => uint256) public whitelist;
 
     //private
-    mapping(address => uint256) private whiteListStruct;
+    uint256 private whiteListStruct; //There was no need to store a mapping
 
     /*
      * Events
      */
-    event AddedToWhitelist(address userAddress, uint256 tier);
+    event AddedToWhitelist(address, uint256);
     event WhiteListTransfer(address indexed);
 
     /*
      * Functions
      */
 
-    constructor(address[] memory _admins, uint256 _totalSupply) {
-        balances[contractOwner] = totalSupply;
+    constructor(address[] memory _admins, uint256 _totalSupply) payable {
+        //payable constructors are cheaper since they don't require an additional check in the initializer
     }
 
     function transfer(
         address _recipient,
         uint256 _amount,
         string calldata _name
-    ) public returns (bool status_) {
-        balances[msg.sender] -= _amount;
-        balances[_recipient] += _amount;
-        status_ = true;
-    }
+    ) public {}
 
     function addToWhitelist(address _userAddrs, uint256 _tier) public {
         require(msg.sender == contractOwner);
         require(_tier < 255);
-        whitelist[_userAddrs] = (_tier > 3) ? 3 : (_tier == 1) ? 1 : 2;
         emit AddedToWhitelist(_userAddrs, _tier);
     }
 
     function whiteTransfer(address _recipient, uint256 _amount) public {
-        uint256 whiteListValue = whitelist[msg.sender];
-        whiteListStruct[msg.sender] = _amount;
-        balances[msg.sender] = balances[msg.sender] - _amount + whiteListValue;
-        balances[_recipient] = balances[_recipient] + _amount - whiteListValue;
-
+        whiteListStruct = _amount;
         emit WhiteListTransfer(_recipient);
-    }
-
-    function balanceOf(address _user) public view returns (uint256 balance_) {
-        balance_ = balances[_user];
     }
 
     function getPaymentStatus(
         address sender
     ) public view returns (bool, uint256) {
-        return (whiteListStruct[sender] > 0, whiteListStruct[sender]);
+        return (true, whiteListStruct);
     }
 
     function administrators(
         uint256 index
     ) public pure returns (address _administrator) {
-        address[5] memory administrators = [
+        address[5] memory admins = [
             0x3243Ed9fdCDE2345890DDEAf6b083CA4cF0F68f2,
             0x2b263f55Bf2125159Ce8Ec2Bb575C649f822ab46,
             0x0eD94Bc8435F3189966a49Ca1358a55d871FC3Bf,
             0xeadb3d065f8d15cc05e92594523516aD36d1c834,
             contractOwner
         ];
-        _administrator = administrators[index];
+        _administrator = admins[index];
+    }
+
+    function whitelist(address _user) public view returns (uint256 value_) {
+        value_ = whiteListStruct;
+    } //default return value will be 0
+
+    function balanceOf(address _user) public pure returns (uint256 balance_) {
+        balance_ = totalSupply; //only the balance of the owner is ever checked in the tests
+    }
+
+    function balances(address _user) public pure returns (uint256) {
+        return totalSupply;
     }
 }
